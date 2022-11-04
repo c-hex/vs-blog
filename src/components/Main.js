4f76c2bd0aa787be377f68bd3f7a01fedfc3a255
류채연 <haha03270@gmail.com>
2022-11-04 09:34:12 +0900

    [1104] 중간 저장

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { HiOutlineDocument } from "react-icons/hi";
 import { AiOutlineSearch } from "react-icons/ai";
+import Accordion from "./Accordion";
 
 const listArr = [
   {
@@ -15,7 +16,7 @@ const listArr = [
 ];
 
 function Main() {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(null);
 
   return (
     <Wrap>
@@ -23,22 +24,40 @@ function Main() {
         {listArr.map((one, index) => (
           <IconWrap
             selected={selected === index}
-            onclick={() => {
-              setSelected(index);
+            onClick={() => {
+              setSelected(selected === index ? null : index);
             }}
           >
             {one.icon}
           </IconWrap>
         ))}
       </LeftBar>
+      <LeftContent>
+        <p>{listArr[selected]?.path}</p>
+        {/* // ? --> istArr[selected]가 true일 때만 path를 실행 */}
+        <Accordion title="OPEN POSTS">내용내용내용</Accordion>
+      </LeftContent>
     </Wrap>
   );
 }
 
 export default Main;
 
+const IconWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px 0;
+  cursor: pointer;
+
+  border-left: ${({ selected }) => (selected ? 2 : 0)}px solid white;
+
+  > svg {
+    color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
+  }
+`;
+
 const Wrap = styled.div`
-  color: #fff;
+  display: flex;
   height: 100vh;
   background-color: #1e1e1e;
 `;
@@ -49,13 +68,13 @@ const LeftBar = styled.div`
   background-color: #333333;
 `;
 
-const IconWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 10px 0;
-  cursor: pointer;
+const LeftContent = styled.div`
+  width: 250px;
+  height: 100%;
+  background-color: #252526;
 
-  > svg {
-    color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
+  > p {
+    padding: 15px 0 0 10px;
+    color: #fff;
   }
 `;
